3e8acf413433209991f05cc1a0e91207970094cc
Alistair Thomson <alistair.t3@gmail.com>
2021-03-29 13:52:41 +0100

    Made UX improvements to the email submissions form

--- app.js
+++ app.js
@@ -2,17 +2,28 @@
 
 $(document).ready(() => {
   $('#mc-embedded-subscribe-form').submit((event) => {
-    alert('wow');
-    console.log("hey")
     event.preventDefault();
 
-    submitEmail();
+    $('#email_validation_message').hide();
+    $('#thank_you').hide();
+
+    if($('#mce-EMAIL').val() == "") {
+      $('#email_validation_message').fadeIn();
+    }
+    else {
+      submitEmail();
+
+      $('#submit_text').hide();
+      $('#submit_loading').show();
+    }
 
     return false;
   });
 });
 
 function submitEmail() {
+  $('#mc-embedded-subscribe').prop('disabled', true);
+
   $.ajax({
     type: 'POST',
     url: 'https://zokopay.us1.list-manage.com/subscribe/post-json?u=4d08a2f8ebf774077f219270c&amp;id=72ab9a8e23&c=?',
@@ -22,10 +33,22 @@ function submitEmail() {
     contentType: "application/json; charset=utf-8",
     error: (error) => {
       console.log('Error', error);
+      $('#mc-embedded-subscribe').prop('disabled', false);
+      showThankYou();
     },
     success: (res) => {
       console.log('Success');
       console.log(res);
+      $('#mc-embedded-subscribe').prop('disabled', false);
+      showThankYou();
     }
   });
+}
+
+function showThankYou() {
+  $('#thank_you').fadeIn();
+  $('#mce-EMAIL').val('');
+
+  $('#submit_text').show();
+  $('#submit_loading').hide();
 }
